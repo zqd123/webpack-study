629625d3d18bc3fcd9a5ea963fccc46e9c457d4f
zqd123 <9544850405@qq.com>
2025-09-22 21:43:34 +0800

    feat(webpack): 实现模块缓存机制并优化加载逻辑
    将 require 函数重命名为 __webpack_require，增强语义并实现模块导出结果的缓存机制。
    同时更新 index.js 中的模块引用方式，确保模块加载逻辑的一致性与正确性。

--- 1-5.编译结果分析/src/my-main.js
+++ 1-5.编译结果分析/src/my-main.js
@@ -2,18 +2,32 @@
 // ./src/a.js
 // ./src/index.js
 (function (modules) {
+  //缓存导出的模块结果
+  const moduleExports = {};
     //moduleId就是模块路径
-    function require(moduleId){}
+    function __webpack_require(moduleId){
+      //如果模块已经被加载过了，就直接返回缓存的结果
+      if(moduleExports[moduleId]){
+        return moduleExports[moduleId]
+      }
+      const func = modules[moduleId]
+      const module={exports:{}}
+      func(module, module.exports, __webpack_require)
+      const result = module.exports
+      //缓存模块结果
+      moduleExports[moduleId] = result
+      return result
+    }
     //执行入口模块
-    require("./src/index.js");//require函数相当于是运行一个模块，并得到模块导出结果
+    return __webpack_require("./src/index.js");//require函数相当于是运行一个模块，并得到模块导出结果
 })({
   "./src/a.js": function (module, exports) {
     console.log("module a");
     module.exports = "a";
   },
-  "./src/index.js": function (module, exports, require) {
+  "./src/index.js": function (module, exports, __webpack_require) {
     console.log("index module");
-    const a = require("./src/a.js");
+    const a = __webpack_require("./src/a.js");
     console.log(a);
   },
 });
