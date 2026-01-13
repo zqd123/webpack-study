b5a96d31d71a793c05011bc4302b2d3a5d817669
zqd123 <9544850405@qq.com>
2026-01-13 22:04:44 +0800

    2-3.复制静态资源

---
+++ 2-3.复制静态资源/webpack.config.js
@@ -0,0 +1,29 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    home:"./src/index.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].[chunkhash:5].js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(), 
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",//模板
+    }),
+    new copyWebpackPlugin({
+      patterns: [
+        //复制静态资源的规则
+        {
+          from: "./public",
+          to: "./",
+        },
+      ],
+    }),
+  ],
+};
